a1bf2e80d9663260650935b38ca54fe627973741
arancia313 <marraalan89@gmail.com>
2025-01-17 18:37:34 +0100

    A bunch of upgraded stuff.

---
+++ System/style.svelte
@@ -0,0 +1,24 @@
+<style>
+              body {
+                 font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+                 margin: 0;
+                 padding: 0;
+                 height: 100vh;
+                 display: flex;
+                 align-items: left;
+                 justify-content: center;
+                 background-color: #0f0f0f; /* Black color for better contrast */ 
+              }
+              h1 {
+                            color: #ffffff; /* White color for better contrast */
+                            text-decoration: none;
+              }
+              p {
+                            color: #ffffff; /* White color for paragraphs */
+                            text-decoration: none;
+              }
+              a {
+                            color:rgb(255, 81, 0); /* Orange color for links */
+                            text-decoration: underline; 
+              }
+           </style>
